3f317f0db053233aa28b4cd447562a81d9892417
xinlvying <zll307339489@live.com>
2019-11-13 14:08:32 +0800

    save

--- index.ts
+++ index.ts
@@ -3,8 +3,20 @@ import Application = require("koa")
 const Koa = require("koa")
 const app: Application = new Koa()
 
-app.use(async (ctx: Application.BaseContext) => {
-  ctx.body = "wx_dev_test"
+app.use(async ctx => {
+  const url = ctx.url
+  // 从上下文的request对象中获取
+  let request = ctx.request
+  let req_query = request.query
+  let req_querystring = request.querystring
+
+  // 从上下文中直接获取
+  let ctx_query = ctx.query
+  let ctx_querystring = ctx.querystring
+
+  ctx.body = {
+    echostr: req_query.echostr
+  }
 })
 
 app.listen(3000)
